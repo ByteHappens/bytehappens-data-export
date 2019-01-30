63da1f4868b25d92971b0f6d19565edff52ff791
Ewan Burns <ewan@beezup.com>
2019-01-30 17:53:40 +0100

    Adding all products

--- src/apps/web/routes/dataexportroute.ts
+++ src/apps/web/routes/dataexportroute.ts
@@ -19,6 +19,96 @@ export class DataExportRoute extends BaseSimpleGetExpressRoute {
         "In Stock": "No",
         Stock: 0,
         "Delivery (days)": 99
+      },
+      {
+        Sku: "EBU_002",
+        Title: "ByteHappens T Shirt (Blue)",
+        "Short Description": "ByteHappens T Shirt",
+        "Long Description": "A ByteHappens T Shirt. Nuf said.",
+        Price: 24.99,
+        "In Stock": "No",
+        Stock: 0,
+        "Delivery (days)": 99
+      },
+      {
+        Sku: "EBU_003",
+        Title: "ByteHappens T Shirt (Pink)",
+        "Short Description": "ByteHappens T Shirt",
+        "Long Description": "A ByteHappens T Shirt. Nuf said.",
+        Price: 24.99,
+        "In Stock": "No",
+        Stock: 0,
+        "Delivery (days)": 99
+      },
+      {
+        Sku: "EBU_004",
+        Title: "ByteHappens T Shirt (Purple)",
+        "Short Description": "ByteHappens T Shirt",
+        "Long Description": "A ByteHappens T Shirt. Nuf said.",
+        Price: 24.99,
+        "In Stock": "No",
+        Stock: 0,
+        "Delivery (days)": 99
+      },
+      {
+        Sku: "EBU_005",
+        Title: "ByteHappens T Shirt (Red)",
+        "Short Description": "ByteHappens T Shirt",
+        "Long Description": "A ByteHappens T Shirt. Nuf said.",
+        Price: 24.99,
+        "In Stock": "No",
+        Stock: 0,
+        "Delivery (days)": 99
+      },
+      {
+        Sku: "EBU_006",
+        Title: "ByteHappens T Shirt (Orange)",
+        "Short Description": "ByteHappens T Shirt",
+        "Long Description": "A ByteHappens T Shirt. Nuf said.",
+        Price: 24.99,
+        "In Stock": "No",
+        Stock: 0,
+        "Delivery (days)": 99
+      },
+      {
+        Sku: "EBU_007",
+        Title: "ByteHappens T Shirt (Black)",
+        "Short Description": "ByteHappens T Shirt",
+        "Long Description": "A ByteHappens T Shirt. Nuf said.",
+        Price: 24.99,
+        "In Stock": "No",
+        Stock: 0,
+        "Delivery (days)": 99
+      },
+      {
+        Sku: "EBU_008",
+        Title: "ByteHappens T Shirt (White)",
+        "Short Description": "ByteHappens T Shirt",
+        "Long Description": "A ByteHappens T Shirt. Nuf said.",
+        Price: 24.99,
+        "In Stock": "No",
+        Stock: 0,
+        "Delivery (days)": 99
+      },
+      {
+        Sku: "EBU_009",
+        Title: "ByteHappens T Shirt (Rainbow)",
+        "Short Description": "ByteHappens T Shirt",
+        "Long Description": "A ByteHappens T Shirt. Nuf said.",
+        Price: 24.99,
+        "In Stock": "No",
+        Stock: 0,
+        "Delivery (days)": 99
+      },
+      {
+        Sku: "EBU_010",
+        Title: "ByteHappens T Shirt (Transparent)",
+        "Short Description": "ByteHappens T Shirt",
+        "Long Description": "A ByteHappens T Shirt. Nuf said.",
+        Price: 24.99,
+        "In Stock": "No",
+        Stock: 0,
+        "Delivery (days)": 99
       }
     ];
 
